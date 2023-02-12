a6a23e4910d1c4ecba65c60b09b92ff295d225c8
Konicai <71294714+Konicai@users.noreply.github.com>
2023-02-11 22:28:59 -0500

    Return false when attempting to send a plugin message to a null player

--- src/main/java/me/konicai/floodgate/pluginmessage/MinestomPluginMessageUtils.java
+++ src/main/java/me/konicai/floodgate/pluginmessage/MinestomPluginMessageUtils.java
@@ -1,7 +1,6 @@
 package me.konicai.floodgate.pluginmessage;
 
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.entity.Player;
 import org.geysermc.floodgate.platform.pluginmessage.PluginMessageUtils;
 
 import java.util.UUID;
@@ -11,10 +10,7 @@ public class MinestomPluginMessageUtils extends PluginMessageUtils {
     @Override
     public boolean sendMessage(UUID player, String channel, byte[] data) {
         try {
-            Player minestomPlayer = MinecraftServer.getConnectionManager().getPlayer(player);
-            if (minestomPlayer != null) {
-                minestomPlayer.sendPluginMessage(channel, data);
-            }
+            MinecraftServer.getConnectionManager().getPlayer(player).sendPluginMessage(channel, data);
         } catch (Exception e) {
             e.printStackTrace();
             return false;
